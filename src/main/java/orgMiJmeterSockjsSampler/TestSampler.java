747b73358123a0ead3ecbd008e7e8252167b020a
frank.mohr <frank.mohr@movingimage.com>
2018-06-20 17:08:15 +0200

    delete passwords

--- src/main/java/orgMiJmeterSockjsSampler/TestSampler.java
+++ src/main/java/orgMiJmeterSockjsSampler/TestSampler.java
@@ -1,7 +1,6 @@
 package orgMiJmeterSockjsSampler;
 
 import java.net.URI;
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,21 +9,28 @@ import java.util.Map;
 import javax.net.ssl.SSLContext;
 
 import org.apache.tomcat.websocket.Constants;
-import org.apache.tomcat.websocket.WsWebSocketContainer;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.RestTemplateXhrTransport;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.SockJsUrlInfo;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+import org.springframework.web.socket.sockjs.frame.Jackson2SockJsMessageCodec;
+import org.springframework.web.socket.sockjs.transport.TransportType;
+
 import javax.net.ssl.TrustManager;
 
 public class TestSampler {
 	
-	private String transport = "websocket";
+	private String transport = "xhr-streaming";
     private String host = "https://xxx.com";
     private String path = "/xxx/";
     private long connectionTime = 500;
@@ -34,23 +40,8 @@ public class TestSampler {
     private String connectionHeadersHost = "host:xxx";
     private String connectionHeadersAcceptVersion = "accept-version:1.1,1.0";
     private String connectionHeadersHeartbeat = "heart-beat:0,0";
-    private String subscribeHeadersId = "id:sub-x";
-    private String subscribeHeadersDestination = "destination:/exchange/xxx/xxx";
-		
-	/*
-    private String transport = "websocket";
-    private String host = "https://wbc-service.movingimage.com";
-    private String path = "/stomp";
-    private long connectionTime = 500;
-    private long responseBufferTime = 10000;
-    private String connectionHeadersLogin = "login:wbc-consumer-prod";
-    private String connectionHeadersPasscode = "passcode:wbc-consumer";
-    private String connectionHeadersHost = "host:webcast_prod";
-    private String connectionHeadersAcceptVersion = "accept-version:1.1,1.0";
-    private String connectionHeadersHeartbeat = "heart-beat:0,0";
     private String subscribeHeadersId = "id:sub-0";
-    private String subscribeHeadersDestination = "destination:/exchange/question-consumer/wbc_59dba188340925066669f7fa";
-    */
+    private String subscribeHeadersDestination = "destination:/exchange/xxx/xxx";
 	
 	public static void main(String[] args)
     {
@@ -58,7 +49,12 @@ public class TestSampler {
 		ResponseMessage responseMessage = new ResponseMessage();
 		
 		try {
-			test.createWebsocketConnection(responseMessage);
+			if (test.transport == "xhr-streaming") {
+				test.createXhrStreamingConnection(responseMessage);
+			} else {
+				test.createWebsocketConnection(responseMessage);
+			}
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -131,6 +127,55 @@ public class TestSampler {
 
  		responseMessage.addMessage(messageProblems);
  	}
+	
+	// XHR-Streaming test
+    public void createXhrStreamingConnection(ResponseMessage responseMessage) throws Exception {
+ 	  RestTemplate restTemplate = new RestTemplate();	  
+ 	  RestTemplateXhrTransport transport = new RestTemplateXhrTransport(restTemplate);
+ 	  transport.setTaskExecutor(new SyncTaskExecutor());
+ 	  
+ 	  SockJsUrlInfo urlInfo = new SockJsUrlInfo(new URI(this.host + this.path));
+ 	  HttpHeaders headers = new HttpHeaders();
+ 	  SockJsXhrTransportRequest request = new SockJsXhrTransportRequest(
+ 		  urlInfo, 
+ 		  headers, 
+ 		  headers,
+ 		  transport, 
+ 		  TransportType.XHR, 
+ 		  new Jackson2SockJsMessageCodec()
+ 	  );
+ 	  SockJsXhrSessionHandler xhrSessionHandler = new SockJsXhrSessionHandler(
+ 		  this.getConnectionsHeaders(), 
+ 		  this.getSubscribeHeaders(), 
+ 		  this.connectionTime,
+		  this.responseBufferTime,
+		  responseMessage
+ 	  );
+      
+ 	  String startMessage = "\n[Execution Flow]"
+					     + "\n - Opening new connection"
+						 + "\n - Using response message pattern \"a[\"CONNECTED\""
+						 + "\n - Using response message pattern \"a[\"MESSAGE\\nsubscription:sub-0\""
+						 + "\n - Using disconnect pattern \"\"";
+
+ 	  responseMessage.addMessage(startMessage);
+ 	  
+ 	  System.out.println(startMessage);
+ 	  
+ 	  transport.connect(request, xhrSessionHandler);
+ 	 
+ 	  String messageVariables = "\n[Variables]"
+ 							  + "\n" + " - Message count: " + responseMessage.getMessageCounter();
+
+ 	  responseMessage.addMessage(messageVariables);
+
+ 	  String messageProblems = "\n[Problems]"
+ 						     + "\n" + responseMessage.getProblems();
+
+ 	  responseMessage.addMessage(messageProblems);
+ 	  
+ 	  System.out.println(messageProblems);
+   }
 	
 	private String getSubscribeHeaders() {
     	return String.format(
